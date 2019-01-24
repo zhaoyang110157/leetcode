792e44d4261a6a4852f1568948653f3cf20c5d7b
朱朝阳 <1853935753@qq.com>
2019-01-24 14:21:01 +0800

    Update solution.h

--- 10-Regular Expression Matching/solution.h
+++ 10-Regular Expression Matching/solution.h
@@ -21,3 +21,15 @@ public:
 	
     }
 };
+
+/*
+This problem has a typical solution using Dynamic Programming. We define the state P[i][j] to be true if s[0..i) matches p[0..j)and false otherwise. Then the state equations are:
+
+P[i][j] = P[i - 1][j - 1], if p[j - 1] != '*' && (s[i - 1] == p[j - 1] || p[j - 1] == '.');
+P[i][j] = P[i][j - 2], if p[j - 1] == '*' and the pattern repeats for 0 times;
+P[i][j] = P[i - 1][j] && (s[i - 1] == p[j - 2] || p[j - 2] == '.'), if p[j - 1] == '*' and the pattern repeats for at least 1 times.
+
+
+原文：https://blog.csdn.net/gao1440156051/article/details/52212918 
+
+*/
