4a8ce429f19c0ed8a10fb095dbb4236fb95bf182
zhaoyang110157 <1853935753@qq.com>
2019-12-06 22:18:45 +0800

    leetcode _day2

--- 27/solution.h
+++ 27/solution.h
@@ -14,3 +14,17 @@ public:
         return size;
     }
 };
+
+class Solution1 {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int temp = 0;
+        for(int i = 0 ; i < nums.size(); i++){
+            if(nums[i] != val){
+                nums[temp] = nums[i];
+                temp++;
+            }
+        }
+        return temp;
+    }
+};
